2a921c052b81e119ae2bb09174a15d96d46400e0
刘谨华 <3104587339@qq.com>
2019-01-18 10:50:19 +0800

    添加删除成绩

--- main.c
+++ main.c
@@ -1,34 +1,61 @@
 #include <stdio.h>
-struct Student {
+
+struct Student{
     int yuwen;
     int shuxue;
     int yingyu;
     char name[100];
 };
+struct Student arr [100];
+int index = 0;
 
 int main(){
 
-    printf("xueshengchengjiguanli");
+    printf("xueshengchengjiguanli\n");
 
     while(1){
-        printf("1---tianjiaxueshengchengji\n");
-        printf("2---shanchuxueshengchengji\n");
-        printf("3---xianshisuoyouchengji\n");
-        printf("4---yewu1,suoyouzongfenbujigedexuesheng(<180)\n");
-        printf("5---yewu2,meiyikedoubujigedexuesheng\n");
-        printf("6---yewu3,xianshizongfenzuigaodexuesheng\n");
-        printf("7---yewu4,xianshipingjunfenzuigao,qiemeiyoubujigedexuesheng\n");
-        printf("8---tuichu\n");
-        printf("qxz\n");
+        printf("1----tianjiaxueshnrgchengji\n");
+        printf("2----shanchuxueshengchengji\n");
+        printf("3----xianshisuoyou\n");
+        printf("4----yewu1,syoyouzongfenbujigedexuesheng(<180)\n");
+        printf("5----yewu2,meiyikedoubujigedexuesheng\n");
+        printf("6----yewu3,xianshizongfenzuigaodexuesheng\n");
+        printf("7----yewu4,xianshipingjunfenzuigao,qiemeiyoubujigedexuesheng\n");
+        printf("8----Tc\n");
+
+        printf("Qxz\n");
 
         int code;
         scanf("%d",&code);
 
         if(code == 1){
+            printf("Qsrxsxm\n");
+            scanf("%s",arr[index].name);
 
+            printf("Qsrxsywcj\n");
+            scanf("%d",&(arr[index].yuwen));
+
+            printf("Qsrxssxcj\n");
+            scanf("%d",&(arr[index].shuxue));
+
+            printf("Qsrxsyycj\n");
+            scanf("%d",&(arr[index].yingyu));
+            index++;
         }
         if(code == 2){
-            
+            if(index > 0){
+                index--;
+                printf("shanchuchenggong,dianjihuichejixu\n");
+                char x;
+                scanf("%c",&x);
+                scanf("%c",&x);
+            }
+            else{
+                printf("wuxueshengxinxi,shanchushibai,dianjihuichejixu\n");
+                char x;
+                scanf("%c",&x);
+                scanf("%c",&x);
+            }
         }
         if(code == 3){
             
@@ -46,7 +73,8 @@ int main(){
             
         }
         if(code == 8){
-            
+            printf("tuichu\n");
+            break;
         }
     }
 
