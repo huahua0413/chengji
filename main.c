cb7615694b05f1069d17c7cb4a8e12c196f107cc
刘谨华 <3104587339@qq.com>
2019-01-18 11:32:56 +0800

    业务3

--- main.c
+++ main.c
@@ -93,7 +93,20 @@ int main(){
             scanf("%c",&x);
         }
         if(code == 6){
-            
+            int sum = 0;
+            int max = 0;
+            int ID;
+            for(int i = 0; i < index; i++){
+                if(max < sum){
+                    max = sum;
+                }
+                ID = i + 1;
+            }
+            printf("di%dmingxuesheng%sdezonghechengjizuigao,zuigaowei%d\n",ID,arr[index].name,max);
+            printf("shaixuanwancheng,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
         }
         if(code == 7){
             
